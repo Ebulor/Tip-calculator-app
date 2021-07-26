9641bf4210d463c4d7574c836a6404a8a7e66b6c
Ebulor <78680916+Ebulor@users.noreply.github.com>
2021-07-26 13:42:00 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,62 @@
+const buttons = document.querySelectorAll(".tip-buttons button");
+const tipAmount = document.querySelector(".amount input");
+const form = document.querySelector("form");
+const tipTotal = document.querySelector(".output-total");
+const tipPerson = document.querySelector(".output-person");
+const numberOfPersons = document.querySelector(".people input");
+const errors = document.querySelector(".people");
+const reset = document.querySelector(".reset");
+const customTipamount = document.querySelector(".tip-buttons input");
+const bill = parseFloat(tipAmount.value);
+const persons = parseFloat(numberOfPersons.value);
+
+
+
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+})
+
+customTipamount.addEventListener("input", ()=>{  
+  tipAmount.value = parseFloat(tipAmount.value);
+  const bill = parseFloat(tipAmount.value);
+  const persons = parseFloat(numberOfPersons.value);  
+  const customTip = parseFloat(customTipamount.value) / 100;  
+  tipPerson.textContent = `$${((bill * customTip) / persons).toFixed(2)}`;
+  tipTotal.textContent = `$${(((bill * customTip) / persons) + bill / persons).toFixed(2)}`;  
+})
+
+buttons.forEach((button)=>{
+    button.addEventListener("click", ()=>{
+        button.classList.add("clicked");
+        const tipPercentage = parseFloat(button.innerText) / 100;  
+        const bill = parseFloat(tipAmount.value);
+        console.log(bill)
+        //to check if a number is inputed
+        if (isNaN(bill) || bill < 1) {
+          tipPerson.textContent = "$0.00";
+          tipTotal.textContent = "$0.00";
+          return;
+        }
+        //check number of persons
+        const persons = parseFloat(numberOfPersons.value); 
+        if(persons == 0 || persons == "" || isNaN(persons)){
+          errors.classList.add("error");
+          return;
+        }
+        else{
+          errors.classList.remove("error");
+        }
+        
+        tipPerson.textContent = `$${((bill * tipPercentage) / persons).toFixed(2)}`;
+        tipTotal.textContent = `$${(((bill * tipPercentage) / persons) + bill / persons).toFixed(2)}`;
+      })
+})
+
+reset.addEventListener("click", ()=>{
+  tipPerson.textContent = "$0.00";
+  tipTotal.textContent = "$0.00";
+  tipAmount.value = "";  
+  numberOfPersons.value = "";
+  customTipamount.value = "";  
+})
+
